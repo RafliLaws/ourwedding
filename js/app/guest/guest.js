197aa71dc18a293050dc295a220697e0c87056b3
RafliLaws <raffsnovo1@gmail.com>
2025-11-19 15:51:35 +0700

    Confetti at DROP

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -171,6 +171,7 @@ export const guest = (() => {
 
         confetti.basicAnimation();
         util.timeOut(confetti.openAnimation, 1500);
+        util.timeOut(confetti.openAnimation, 46000);
 
         document.dispatchEvent(new Event('undangan.open'));
         util.changeOpacity(document.getElementById('welcome'), false).then((el) => el.remove());
