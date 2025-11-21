f46b3d7d250b2227569241487d281ba2bfecf420
RafliLaws <raffsnovo1@gmail.com>
2025-11-21 08:55:19 +0700

    Ubah komen supaya gk di share

--- js/app/components/comment.js
+++ js/app/components/comment.js
@@ -36,8 +36,8 @@ export const comment = (() => {
      */
     const onNullComment = () => {
         const desc = lang
-            .on('id', '📢 Yuk, share undangan ini biar makin rame komentarnya! 🎉')
-            .on('en', '📢 Let\'s share this invitation to get more comments! 🎉')
+            .on('id', 'Terima kasih atas Ucapan dan Doanya!!!! 🎉')
+            .on('en', 'Terima kasih atas Ucapan dan Doanya!!!! 🎉')
             .get();
 
         return `<div class="text-center p-4 mx-0 mt-0 mb-3 bg-theme-auto rounded-4 shadow"><p class="fw-bold p-0 m-0" style="font-size: 0.95rem;">Terima kasih atas Ucapan dan Doanya!!!!</p></div>`;
